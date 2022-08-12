73774c36dbdc40cfb9fa7293a006d6d48507cc71
Reza Saker <reza.shossain@gmail.com>
2022-08-12 11:41:20 -0500

    New Kattis problems

---
+++ Kattis/3DPrintedStatues.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, d, p, s;
+
+    cin>>n;
+
+    d=0;
+    p=1;
+
+    while((p*2)<n)
+    {
+        d++;
+        p *= 2;
+    }
+
+    s=0;
+
+    while(s < n)
+    {
+        s += p;
+        d++;
+    }
+
+    cout<<d;
+
+    return 0;
+}
